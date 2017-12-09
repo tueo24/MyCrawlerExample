67e8f97e1cff071bda32ea9f42c88ba7e39ffc95
tueo24 <tueo24@gmail.com>
2017-12-09 15:02:53 +0800

    20171209

--- src/crawler/example/FacebookExample.java
+++ src/crawler/example/FacebookExample.java
@@ -27,7 +27,7 @@ public class FacebookExample {
 		String uri = 
 				"https://graph.facebook.com/v2.5"
 				+ "/search?q=%E9%9D%A0%E5%8C%97&type=page&limit=1000&fields=name,id,likes,talking_about_count"
-				+ "&access_token=EAACEdEose0cBAMzLyMlMxZAY73J1TBiZAHJhrF8du8fLhMmGYrUA3W9ezJDQk9oZAa07o4F2drZAL2k1cCtgdpl5LZClZCo6733A3xddNbjSAoTxEZBWY4rKnzLWTU3I0KMgT0qsw6eIRfdzg9RZCIkuTZCtGZCyzct657fywQpsFCNAZDZD";
+				+ "&access_token=EAACEdEose0cBAGqWzbcZBFy5BMAUZAorTonDJAjoF4hNxvURZC2Q3kooJmBgZA8jxbAYf3yIOACYerRAEbJT36knOdrvvgSY5dwZCkBpWlOqZAjNNcF8MlFg7mBQ3M7KHrltKfW2vBpuSA5JZC6LaCd0ntQoppS1dX7kTC0MU02hmYyUOSmZAEZBIUccEjlgQv0UZD";
 
 
 
