6073ce8323b87b302947e1b13b381661e683c99a
tueo24 <tueo24@gmail.com>
2017-12-10 09:19:07 +0800

    20171209

--- src/crawler/example/exam/FacebookExam.java
+++ src/crawler/example/exam/FacebookExam.java
@@ -25,8 +25,8 @@ public class FacebookExam {
 
 		String uri = 
 				"https://graph.facebook.com/v2.6"
-				+ "/search?q="
-				+ "&access_token=";
+				+ "/EBCbuzz/posts?fields=id,link,message,created_time,likes.limit(0).summary(total_count),reactions.limit(0).summary(total_count)"
+				+ "&access_token=EAACEdEose0cBALgIRMZAhHXioCifaaXyKBmKtWKYgT7jjOxM9UbbrIzZA5cCIw8it15qz4ZBzVWwZCwQ9ZBSnAdt3LOXa1YMf7XrFSTIPFN528ZCViZCajq57dPNZAdWiDbM7vTF1tWrD41HmSNuD3h073KdyaDB9oIvGAyA9MmtnaZC1Kt81S4h5oE6sVW8CZBvdo2iQpggqhQoZCZCBss7qBeDEH3yYmAFGH4ZD";
 
 
 		Elements elems =
